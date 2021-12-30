a8106a0a07af79d6f794f95de095bf4af101c1e2
IA-Masum <iamasum133@gmail.com>
2021-12-30 20:51:18 +0600

    Javascript class_01 variables and String

---
+++ JavaScript/class_01.js
@@ -0,0 +1,76 @@
+// var myName;
+// var myAge;
+
+// myName = "Imran";
+// myAge = 29;
+
+// console.log(myName);
+// console.log(myAge);
+
+// var myName = "Imran";
+// var myAge = 29;
+
+// console.log(myName);
+// console.log(myAge);
+
+// a-z, A-Z, 0-9, $,_
+
+var myName, myAge, myHeight, amIBlack;
+
+myName = "Imran";
+myAge = 29;
+myHeight = 5.6;
+amIBlack = false;
+
+// console.log( myName, myAge, myHeight, amIBlack)
+
+// Data Types
+// 1. String, "", ''
+// 2. Number -> Integer, Float
+// 3. Boolean -> true, false
+
+// var str = 'I\'m a student.';
+// var str2 = `I'am a student. He said me "go home"`
+// console.log(str);
+
+
+// I don't have any cow.
+
+// var pass = "9urejfoaioufoiwJRCV";
+
+// var char = pass.length;
+
+// console.log(char);
+
+// var fName = "Imran";
+// var mName = "Ali";
+// var lName = "Masum";
+
+// // var fullname = fName + " " + mName + " " + lName;
+// var fullname = `${fName} ${mName} ${lName}`;
+
+// console.log(fullname);
+
+
+// var output = 'My Contry: , My City: , My Village: ';
+
+
+// var country =  "Bangladesh";
+// var city = "Rajshahi";
+// var village = "Shibpur";
+
+// var output = `My Country: ${country}, My City: ${city}, My Village: ${village}`;
+
+// console.log(output);
+
+// My Name: , My Age: , My Height: 
+
+// var str = 'Hello! My Name is Masum. Masum name is mine.';
+
+// var str2 = str.replaceAll('Masum', 'Anas');
+
+// console.log(str2);
+
+// var str = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis sit reprehenderit velit. Ut amet autem voluptas voluptatem dignissimos. Porro tempora mollitia, consequuntur perspiciatis quibusdam commodi fugit dolorum doloribus temporibus? In incidunt dolore alias recusandae similique ex nesciunt excepturi necessitatibus quidem autem, reprehenderit nobis inventore dolor aut corrupti earum ratione laboriosam?"
+
+// console.log(str.length)
