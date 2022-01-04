56f19205cd180e7156e9c89f1fd4f89b37cd44fb
IA-Masum <iamasum133@gmail.com>
2022-01-04 17:17:52 +0600

    Js class 03 done

---
+++ JavaScript/class_02.js
@@ -0,0 +1,112 @@
+// Javascirpt Operators
+// 5 + 6;
+
+// Arithmetic Operators
+
+// 1. addition 		    + 	5 + 2 -> 7
+// 2. substruction 	    - 	5 - 2 -> 3
+// 3. multiplication 	* 	5 * 2 -> 10 
+// 4. exponentiation 	** 	5 ** 2 -> 25
+// 5. division 		    /  	5 / 2 -> 2.5
+// 6. modulus 		    % 	5 % 2 -> 1
+// 7. increment 		++	5++ -> 6
+// 8. decrement 		--	5-- -> 4
+
+// let num1 = 20;
+// let num2 = 21;
+// num1--;
+// num1--;
+// console.log( ++num1);
+// console.log( num1);
+
+// let men = 500;
+// let row = 13;
+// let ovagas = men % row;
+// let vagas = men - ovagas;
+
+// console.log(158 % 16 == 0);
+
+
+	
+// Assignment Operators
+
+// 1. =	  x = y	   x = y
+// 2. +=	  x += y	   x = x + y
+// 3. -=	  x -= y	   x = x - y
+// 4. *=	  x *= y	   x = x * y
+// 5. /=	  x /= y	   x = x / y
+// 6. %=	  x %= y	   x = x % y
+// 7. **=	  x **= y	   x = x ** y
+// and more
+
+// let num1 = 5;
+
+// num1 += 10;
+
+// console.log(num1)
+
+// String operators
+
+// 1. =	  x = y	   x = y
+// 2. +=	  x += y	   x = x + y
+
+// let str = 'Imran ';
+// let str2 = 'Ali';
+
+// str += str2;
+
+// console.log(str);
+
+// let result = 5 > 6;
+// console.log(result)
+
+
+// Comparison Operators
+
+// 1. = = 	equal to
+// 2. = = =	equal value and equal type
+// 3. ! =	not equal
+// 4. ! = =	not equal value or not equal type
+// 5. >	greater than
+// 6. <	less than
+// 7. > =	greater than or equal to
+// 8. < =	less than or equal to
+// 9. ?	ternary operator
+
+// let num1 = 5;
+// let num2 = '5';
+
+// console.log(num1 !== num2)
+
+// Logical Operators
+
+// 1. &&	logical and
+// 2. ||	logical or
+// 3. !	logical not
+
+// console.log( !true );
+
+// 5 -> 0 1 10 11 100, 101, 110, 111
+// & |
+
+// 101
+//  10
+// 000 -> 7
+
+// console.log(5 << 3)
+
+
+
+
+// let readline = require('readline');
+
+// let rl = readline.createInterface(process.stdin, process.stdout);
+
+// rl.question('Enter Your Age. ', input => {
+
+//     let result = input >= 18 ? 'Welcome' : 'Get Out!';
+//     console.log(result);
+
+//     rl.close()
+// })
+
---
+++ JavaScript/class_03.js
@@ -0,0 +1,100 @@
+
+
+
+// let num1 = 5;
+// let num2 = 10;
+// let num3 = 15;
+// let num4 = 20;
+
+
+// let result = add(num1, num2);
+// let result = add(num1, num3);
+
+
+// function add(n1, n2){
+//     return n1 + n2;
+// }
+
+
+// console.log(result);
+
+
+// function myFunction(){
+//     console.log("I AM MY Function!");
+// }
+
+
+// myFunction();
+
+
+// function sayHello(name = 'Guest'){
+//     console.log("Hello "+name+" !")
+// }
+
+// let myName= "Maruf";
+
+// sayHello(myName);
+// sayHello("Rizvi");
+// sayHello();
+
+// function add(a, b){
+//     return a + b;
+// }
+
+// function sub(a, b){
+//     return a - b;
+// }
+
+// function multi(a, b){
+//     return a * b;
+// }
+
+// function division(a, b){
+//     return a / b;
+// }
+
+// function modu (a, b){
+//     return a % b;
+// }
+
+// console.log(add(5, 10))
+
+// "Imran" "Masum" -> "Imran Masum"
+
+// function getFullName (fn, ln){
+//     return fn + " " + ln
+// }
+
+// console.log(getFullName('Imran', 'ali'))
+
+
+
+// function getKalvin(cel){
+//     return cel + 273;
+// }
+
+
+// let kelvin = getKalvin(100);
+
+// console.log(kelvin);
+
+// function getArea(width, height){
+//     return width * height
+// }
+
+// function getTriangleArea(land, height){
+//     return .5 * land * height;
+// }
+
+// console.log(1/2, .5)
+
+//
+
+// function printAll(n1 , n2){
+//     console.log(`Addition ${n1 + n2}`)
+//     console.log(`Subtruction ${n1 - n2}`)
+//     console.log(`Multiplication ${n1 * n2}`)
+//     console.log(`Division ${n1 / n2}`)
+//     console.log(`Modulus ${n1 % n2}`)
+// }
+// printAll(50, 31);
