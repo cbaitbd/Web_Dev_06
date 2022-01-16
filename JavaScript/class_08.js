f634dc4cf2e3477fe3d34d2e88b59debeb413c91
IA-Masum <iamasum133@gmail.com>
2022-01-16 20:42:52 +0600

    javascript 8 more about function

---
+++ JavaScript/class_08.js
@@ -0,0 +1,115 @@
+// function myFunc(){
+//     console.log('Something!')
+// }
+
+// var myVar = myFunc;
+
+// // console.log(myVar)
+
+// myVar();
+
+// var myFunc = function(){
+//     console.log("Benami");
+// }
+
+// function mainFunc(func){
+//     console.log("I am from MainFunc");
+//     func();
+// }
+
+// function func2(){
+//     console.log("I am from Func2");
+// }
+
+// mainFunc(func2);
+
+
+// function treat(person, callback){
+
+// };
+
+// var p = {
+//     name: "Arafat",
+//     isStudent: true
+// }
+
+
+
+// setTimeout(function(){
+//     console.log("Another");
+// },2000);
+
+
+// function returnFunc(){
+//     console.log('I am returning a function call it to see!');
+
+//     return function(){
+//         console.log("I Am Returned Function!")
+//     }
+// }
+
+// var ss = returnFunc();
+// ss();
+
+// returnFunc()();
+
+
+// function first(a){
+//     return function (b){
+//         return a + b;
+//     }
+// }
+
+// var num1 = 10;
+// var num2 = 15;
+
+// var second = first(num1);
+// var result = second(num2)
+// var result = first(num1)(num2);
+
+// console.log(result);
+
+// var a = 5;
+// var b = a;
+
+// b = 10;
+
+// console.log(a);
+
+// var ar = [5, 6];
+// var br = [...ar];
+
+// for(let i = 0; i < ar.length; i++){
+    
+//     br.push(ar[i]);
+// }
+
+// br = [...br, 45];
+// console.log(ar);
+
+
+// function add (...arguments){
+//     let result = 0;
+//     for(let i = 0; i < arguments.length; i++){
+//         let val = arguments[i];
+//         result += val;
+//     }
+//     return result;
+// }
+
+// console.log( true == "58");
+
+// 125668
+
+// 0, undefined, null, [];
+// {}
+
+// if([] == true){
+//     console.log("kjfkj");
+// }
+
+
+
+
+
+
