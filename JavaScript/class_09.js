a8ec4ec0f0e2984064a4123cbf8f84e285c52207
IA-Masum <iamasum133@gmail.com>
2022-01-18 17:08:43 +0600

    class_09 JS Scope

---
+++ JavaScript/class_09.js
@@ -0,0 +1,71 @@
+// var a = 10;
+
+// function myFunc(){
+//     var  b ;
+//     // console.log(b)
+//     function inner(){
+//         var c = 30;
+//         d = 40;
+//     }
+//     // inner();
+// }
+
+// myFunc();
+// // inner();
+// console.log(d)
+
+
+// let i = 10;
+// let i = 25;
+
+
+// for(let i = 0; i < 100; i++){
+    // fkalfk
+    
+// }
+
+// console.log(i);
+
+// const a = 10;
+// a = 100;
+
+// const myfunc = function(){
+//     console.log(10);
+// }
+
+
+// myfunc = {};
+
+// myfunc();
+// console.log()
+
+// const obj = {
+//     name: "kicu"
+// }
+
+// obj.age = 25;
+
+// const number = 5;
+// number = 10;
+
+// console.log(obj)
+
+// const arr = [5, 5, 10];
+
+// var a = 10;
+
+// function myFunc(){
+//     a = 20;
+//     function myfunc2(){
+//         var a = 30;
+//         if(true){
+//             let a = 40;
+//         } 
+//     }
+//     myfunc2();
+//     console.log(a);  
+// }
+// myFunc();
+
+
+
